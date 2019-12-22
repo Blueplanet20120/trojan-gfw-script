66271b8555067f6da74ee8cb0c88c6507cf2c5f4
Blue planet <30463977+Blueplanet20120@users.noreply.github.com>
2019-12-22 22:01:31 +0800

    Add delete FILENAME

--- index/camouflage.sh
+++ index/camouflage.sh
@@ -4,8 +4,8 @@ if [ ! -d "/usr/share/nginx/html" ]; then
   mkdir /usr/share/nginx/html
 fi
 cd /usr/share/nginx/html 
-rm -rf *
+rm -rf /usr/share/nginx/html/*
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1k89A3ae8nBFn6wRdh-tfrWUnY5hl_oWX' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1k89A3ae8nBFn6wRdh-tfrWUnY5hl_oWX" -O FILENAME && rm -rf /tmp/cookies.txt
 unzip FILENAME
-rm -rf FILENAME
+rm -rf /usr/share/nginx/html/FILENAME
 exit 0
