3d1e6fc9ad686a4fd8342768e14ff1aefc8cc2ac
Blue planet <30463977+Blueplanet20120@users.noreply.github.com>
2019-12-22 22:15:55 +0800

    Update camouflage-bw.sh

--- index/camouflage-bw.sh
+++ index/camouflage-bw.sh
@@ -4,6 +4,7 @@ if [ ! -d "/usr/share/nginx/html" ]; then
    mkdir /usr/share/nginx/html
 fi
 cd /usr/share/nginx/html
+rm -rf /usr/share/nginx/html/*
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1H8sbwv16PETxLM_zmzVAKYBagP-ats5q' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1H8sbwv16PETxLM_zmzVAKYBagP-ats5q" -O FILENAME && rm -rf /tmp/cookies.txt
 unzip FILENAME
 sleep 3
